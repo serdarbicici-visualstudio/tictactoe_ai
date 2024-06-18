094a3cb8f879af7ed5ef2079c2555ed424397a27
serdarbicici-visualstudio <serdarbicici.visualstudio@gmail.com>
2024-06-19 00:52:41 +0300

    still not working

--- agent.py
+++ agent.py
@@ -2,24 +2,17 @@ from tictactoe_game import Tictactoe
 import random
 
 class Agent(Tictactoe):
-
-    # agent plays player 2 (O)
-
     def __init__(self):
         super().__init__()
         self.q_table = {}
-        # q_table = {"state": [action, Q-value]}
-        # state = "---------"
-        # action = 0-8
-        # Q-value = 0-1
-
-        self.alpha = 0.5
-        self.gamma = 0.5
-        self.epsilon = 0.9
+        self.alpha = 0.1
+        self.gamma = 0.9
+        self.epsilon = 0.1
         self.state = None
         self.action = None
         self.next_state = None
         self.done = None
+        self.all_states = set()
 
     def reset(self):
         super().reset()
@@ -27,128 +20,120 @@ class Agent(Tictactoe):
         self.action = None
         self.next_state = None
         self.done = None
+        self.all_states = set()
 
     def get_state(self):
         return "".join(self.board)
 
     def check_double(self, player):
-
-        # check if player can win in the next move
         for i in range(9):
             if self.board[i] == "-":
-                self.board[i] = player
+
+                if player == self.player1:
+                    self.board[i] = "X"
+
+                else:
+                    self.board[i] = "O"
+
                 if self.winner(player):
                     self.board[i] = "-"
                     return True
+                
                 self.board[i] = "-"
-
         return False
-    
-
-    
-
 
     def get_reward(self):
-
         if self.winner(self.player2):
-            return 10
-        
+            return 1
         if self.winner(self.player1):
-            return -100000
-        
-        #if self.check_double
+            return -1
+        if self.is_draw():
+            return -0.5
+        if self.check_double(self.player1):
+            return -100
+        if self.check_double(self.player2):
+            return 0.5
         
         return 0
 
     def get_possible_actions(self):
         return [i for i in range(9) if self.board[i] == "-"]
-    
+
     def get_action(self, state):
-        if state not in self.q_table.keys():
+        if state not in self.q_table:
             self.q_table[state] = [[action, 0] for action in self.get_possible_actions()]
-        
         if random.random() < self.epsilon:
             return random.choice(self.get_possible_actions())
-        
-        return max(self.q_table[state], key=lambda x: x[1])[0]
+        return max(self.q_table[state], key=lambda x: x[1] if self.board[x[0]] == "-" else float('-inf'))[0]
+
     
+
+    def is_draw(self):
+        return "-" not in self.board
+
+    def save_q_table(self):
+        with open("q_table.csv", "w") as f:
+            for state, actions in self.q_table.items():
+                f.write(state + ",")
+                for action in actions:
+                    f.write(str(action[0]) + "," + str(action[1]) + ",")
+                f.write("\n")
+
     def train(self, episodes):
-    # train the agent to play against player 1
-    # choose only player 2's moves, make player 1 play randomly
         for episode in range(episodes):
             self.reset()
             self.state = self.get_state()
             self.done = False
 
-            for round in range(9):
-                reward = 0  # Initialize reward
+            while not self.done:
+                # Get the current state and select an action using epsilon-greedy policy
+                self.state = self.get_state()
+                self.action = self.get_action(self.state)
 
-                if round % 2 == 0:
-                    player = self.player1
-                    self.action = random.choice(self.get_possible_actions())
+                # Take the action and observe the reward and the next state
+                self.move(self.player2, self.action // 3, self.action % 3)
+                self.next_state = self.get_state()
+                reward = self.get_reward()
 
-                    self.move(player, self.action // 3, self.action % 3)
+                # If the game is done, set the done flag
+                self.done = self.winner(self.player2) or self.is_draw()
 
-                    self.next_state = self.get_state()
-
-                    if self.winner(player):
-                        reward = -1
-                        self.done = True
-
-                else:
-                    player = self.player2
-                    self.action = self.get_action(self.state)
-
-                    self.move(player, self.action // 3, self.action % 3)
-
-                    self.next_state = self.get_state()
-
-                    if self.winner(player):
-                        reward = 1
-                        self.done = True
-
-                if self.done:
-                    if self.state not in self.q_table:
-                        self.q_table[self.state] = [[action, 0] for action in self.get_possible_actions()]
-                    for entry in self.q_table[self.state]:
-                        if entry[0] == self.action:
-                            entry[1] = reward
-                    break
-
+                # Initialize Q-table for next state if it doesn't exist
                 if self.next_state not in self.q_table:
                     self.q_table[self.next_state] = [[action, 0] for action in self.get_possible_actions()]
 
+                # Update the Q-table for the current state-action pair
                 if self.state not in self.q_table:
                     self.q_table[self.state] = [[action, 0] for action in self.get_possible_actions()]
 
-                if len(self.q_table[self.next_state]) > 0:
-                    max_q_next = max(self.q_table[self.next_state], key=lambda x: x[1])[1]
+                current_actions = self.q_table[self.state]
+                action_index = next(index for index, value in enumerate(current_actions) if value[0] == self.action)
+
+                if self.done:
+                    self.q_table[self.state][action_index][1] = reward
                 else:
-                    max_q_next = 0
+                    future_rewards = [q[1] for q in self.q_table[self.next_state] if self.board[q[0]] == "-"]
+                    max_future_reward = max(future_rewards) if future_rewards else 0
+                    q_value = (1 - self.alpha) * self.q_table[self.state][action_index][1] + self.alpha * (reward + self.gamma * max_future_reward)
+                    self.q_table[self.state][action_index][1] = q_value
 
-                for entry in self.q_table[self.state]:
-                    if entry[0] == self.action:
-                        entry[1] += self.alpha * (
-                            reward + self.gamma * max_q_next - entry[1])
+                # If the game isn't done, switch to the opponent's move
+                if not self.done:
+                    opponent_action = random.choice(self.get_possible_actions())
+                    self.move(self.player1, opponent_action // 3, opponent_action % 3)
+                    self.done = self.winner(self.player1) or self.is_draw()
 
-                self.state = self.next_state
+                if episode % 1000 == 0:
+                    print("Episode: ", episode)
 
-            if episode % 10 == 0:
-                print(f"Episode {episode + 1}/{episodes}")
 
     def play_with_human(self):
-        
         self.reset()
         self.draw_game()
-
         for round in range(9):
-
             if round % 2 == 0:
                 player = self.player1
-
                 print("Player 1's turn, enter the position (x, y): ")
-                
-                
                 while True:
                     try:
                         x, y = map(int, input().split())
@@ -156,46 +141,23 @@ class Agent(Tictactoe):
                         break
                     except:
                         print("Invalid move, try again!")
-                    
-
-
                 self.draw_game()
-
                 if self.winner(player):
                     print("Player 1 wins!")
                     break
-
             else:
                 player = self.player2
-
                 print("Player 2's turn, enter the position (x, y): ")
-                
-                # get best action from q_table
                 self.action = self.get_action(self.get_state())
                 self.move(player, self.action // 3, self.action % 3)
-
-
-               
-                    
-
                 self.draw_game()
-
                 if self.winner(player):
                     print("Player 2 (bot) wins!")
                     break
-
-            
-
         print("\nGame Over!")
 
-    
-    
-
-    
-
 if __name__ == "__main__":
-    # train the agent and print output, then play with human
     agent = Agent()
-    agent.train(100000)
-
-    agent.play_with_human()
+    agent.train(5000000)
+    while True:
+        agent.play_with_human()
